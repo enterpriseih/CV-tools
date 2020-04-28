4ef0291544216475451c50a46c1523767ec68d86
Biuke Yang (杨风光) <yangfg@inspur.com>
2020-04-28 13:50:35 +0800

    fix color not fix bug

--- box-colors/opencv_random_color.py
+++ box-colors/opencv_random_color.py
@@ -1,8 +1,13 @@
-import cv2
-import numpy as np
+import random
 
-def get_all_colors(classes):
-    return np.random.uniform(0, 255, size=(len(classes), 3))
+def get_all_colors(classes, seed=2):
+    class_colors = {}
+    random.seed(seed)
+    for cls in classes:
+        class_colors[cls] = [random.randint(0, 255) for _ in range(3)]
+
+    return  class_colors
+
 
 if __name__ == '__main__':
     print(get_all_colors(['person', 'hat']))
